5fa0a57bd582c64d18c312e60314e1454655cb7d
Peter Quispe Mendoza <peterqm@hotmail.com>
2022-02-24 16:01:18 -0500

    Commit inicial

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+#include "freeglut/src/fg_version.h"
+
+int main(int, char**) {
+    std::string n={"Hola, Soy Peter"};
+    std::cout << n<<std::endl;
+    system("pause");
+    return EXIT_SUCCESS;
+}
